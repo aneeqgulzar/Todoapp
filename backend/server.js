fabf4ef738b48f2af97a24d8732ec5183b711325
aneeqgulzar <aneeqgulzaeonline@gmail.com>
2024-01-15 17:10:24 +0500

    changes in package

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,11 @@ const taskRoutes = require('./routes/taskRoutes');
 const app = express();
 // const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: []
+  }
+));
 app.use(express.json());
 
 
